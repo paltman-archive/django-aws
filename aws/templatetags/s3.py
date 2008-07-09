4746dedff03675158ba5926a63b5f3e48087a632
Patrick Altman <paltman@gmail.com>
2008-07-08 23:23:24 -0500

    added initial s3 tags

--- aws/templatetags/s3.py
+++ aws/templatetags/s3.py
@@ -1,10 +1,10 @@
-from django import template
-import re
+from django.template import TemplateSyntaxError, Node, Library
 from boto.s3 import Connection
 
-register = template.Library()
+register = Library()
 
-class GetBuckets(template.Node):
+
+class GetBuckets(Node):
     def __init__(self, var_name):
         self.var_name = var_name
     def render(self, context):
@@ -13,23 +13,61 @@ class GetBuckets(template.Node):
 
 @register.tag
 def get_buckets(parser, token):
-    try:
-        tag_name, arg = token.contents.split(None, 1)
-    except ValueError:
-        raise template.TemplateSyntaxError, \
-                     '%s tag requires arguments.' %  token.contents.split()[0]
-    match = re.search(r'[.]*as (\w+)', arg)
-    if not match:
-        raise template.TemplateSyntaxError, \
-               '%s tag requires an argument of a context variable.' % tag_name
-    var_name = match.groups()[0]
-    return GetBuckets(var_name)
+    """{% get_buckets as [varname] %}"""
+    args = token.contents.split()
+    if len(args) != 3:
+        raise TemplateSyntaxError, "get_buckets takes exactly 2 arguments."
+    if args[1] != 'as':
+        raise TemplateSyntaxError, "get_buckets first argument must be 'as'"
+    return GetBuckets(args[2])
+   
+    
+
+class ListBucket(Node):
+    def __init__(self, bucket, var_name, prefix=None):
+        self.bucket = bucket
+        self.var_name = var_name
+        self.prefix = ''
+        if prefix:
+            self.prefix = prefix
+    def render(self, context):
+        context[self.var_name] = Connection().get_bucket(self.bucket).list(self.prefix)
+        return ''
     
 @register.tag
 def list_bucket(parser, token):
-    pass
+    """{% list_bucket [bucket_name] [prefix] as [varname] %}"""
+    args = token.contents.split()
+    if len(args) not in (4,5):
+        raise TemplateSyntaxError, "list_bucket takes 4 or 5 arguments"
+    if (len(args) == 4 and args[2] != 'as') or \
+       (len(args) == 5 and args[3] != 'as'):
+        raise TemplateSyntaxError, "list_bucket must set a variable with 'as'"
+    if (len(args) == 4):
+        return ListBucket(bucket=args[1].replace('"',''), var_name=args[3])
+    if (len(args) == 5):
+        return ListBucket(bucket=args[1].replace('"',''), prefix=args[2].replace('"',''), var_name=args[4])
 
+
+
+class GetObjectInfo(Node):
+    def __init__(self, bucket, key, var_name):
+        self.bucket = bucket
+        self.key = key
+        self.var_name = var_name
+    def render(self, context):
+        key = Connection().get_bucket(self.bucket).get_key(self.key)
+        print key, key.name, key.size
+        context[self.var_name] = key
+        return ''
 
 @register.tag
 def get_object_info(parser, token):
-    pass
+    """{% get_object_info [bucket_name] [key_name] as [varname] %}"""
+    args = token.contents.split()
+    if len(args) != 5:
+        raise TemplateSyntaxError, "get_object_info takes exactly 5 arguments"
+    if args[3] != 'as':
+        raise TemplateSyntaxError, "get_object_info must set a variable"
+    return GetObjectInfo(bucket=args[1].replace('"',''), key=args[2].replace('"',''), var_name=args[4])
+
