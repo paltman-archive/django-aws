54023f7ccad402ad4e47bee8028235e594560d99
Patrick Altman <paltman@gmail.com>
2010-06-17 01:42:32 -0500

    added some comment notes about cached objects

--- aws/models.py
+++ aws/models.py
@@ -1,8 +1,28 @@
 from django.db import models
 
-class StorageObject(models.Model):
+
+class Bucket(models.Model):
+    """
+    Data about the ``bucket`` object on Amazon S3.
+
+    - name
+    - acl
+    - logging
+    -
+    """
+    pass
+
+
+class Key(models.Model):
     """
     Represents a single Amazon S3 object.
+
+    - bucket
+    - name
+    - acl
+    - size
+    - md5
+    - last_modified
     """
     pass
 
